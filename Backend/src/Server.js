c68ebdfeff9bde594db998522eac50bd35b8ee4d
AnandNarra <narra9505@gmail.com>
2026-02-23 14:26:37 +0530

    feat: initialize Express server with database connection, CORS configuration, and API routes for user, admin, driver, ride, and chat.

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 const allowedOrigin = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://ride-sync-iota.vercel.app/"
+  "https://ride-sync-iota.vercel.app"
 
 
 ];
